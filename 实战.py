c46039c5995d7afd33e18b60cabaa192d62241cc
WHY015 <3069344772@qq.com>
2026-09-25 14:10:44 +0800

    remove practice files from git tracking

--- 实战.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoModel, AutoTokenizer
-
-model = AutoModel.from_pretrained("bert-base-chinese")
-tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
-
-inputs = tokenizer("我爱你", return_tensors="pt")
-outputs = model(**inputs)
-print(outputs.last_hidden_state.shape)
